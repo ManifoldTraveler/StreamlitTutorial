9d058e769fb1c390d6aed28be671af93f8b39975
Sergio Lopez Martinez <70881259+SergioSKA27@users.noreply.github.com>
2023-04-17 11:48:45 -0600

    Add files via upload

---
+++ pages/Select Box.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.header(":red[Select Box]")
+
+
+"""
+Para poder dar una lista de opciones podemos hacer uso de st.selectbox la cual
+nos ayudara a poder dar una lista de valores para elegir. Para ello pasamos como
+argumentos la etiqueta(string), una tupla con las opciones a elegir.
+"""
+
+sb = st.selectbox('Cual es tu color favorito?', ('rojo','azul','verde'))
+c = ''
+
+if sb == 'rojo':
+    c = 'red'
+elif sb == 'azul':
+    c = 'blue'
+elif sb == 'verde':
+    c = 'green'
+
+
+
+
+
+st.write("Tu color favorito es:  :"+c+"["+str(sb)+"]")
---
+++ pages/Sliders.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import time, datetime
+
+
+st.header(':red[Slider]')
+
+
+"""
+Los sliders (deslizadores) nos sirven para poder establecer un modo
+sencillo para dar valores dentro de un rango definido o rangos de valores
+para poder utilizarlos. Para esto hacemos uso de st.slider este toma como
+argumentos la etiqueta del slider, el valor minimo, el valor maximo y el valor
+por default, estos valores pueden ser enteros o flotantes (todos los tipos de datos
+deben de ser iguales). Ademas podemos hacer uso del parametro step el cual nos permite
+establecer como cambian los valores entre cada paso.
+"""
+
+#Ejemplo 1 (Slider para un solo valor)
+age = st.slider('Cual es tu edad?', 0, 130, 25,step=10)
+st.write("Tengo ", age, 'años')
+
+#ejemplo 2 (Slider de rango)
+
+"""
+Podemos establecer sliders para rangos tomando de igual forma los valores de la etiqueta, el valor
+minimo, maximo y el valor por default en este caso sera una tupla con el valor minimo del rango y
+el valor maximo.
+"""
+
+ran = st.slider('Selecciona un rango: ',0.0,10.0,(3.1,5.5))
+st.write("El rango es: ", ran)
+
+
